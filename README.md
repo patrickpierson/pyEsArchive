585debeca3dae6bb408813e6159f0ab6603fb772
patrickpierson <patrick.c.pierson@gmail.com>
2016-01-25 21:58:04 -0500

    got it working with elasticsearch service in aws

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 pip install elasticsearch boto3
 
 python ./pyEsArchive.py --help
+
+python ./pyEsArchive.py --archive test --numdays ## --bucket BUCKET --esaddress search-######.us-east-1.es.amazonaws.com --esport 443 --awsaccesskey ACCESSKEY --awssecretkey SECRETKEY --logtype cwl --dry
--- pyEsArchive.py
+++ pyEsArchive.py
@@ -1,24 +1,40 @@
 from datetime import datetime
-from elasticsearch import Elasticsearch
+from elasticsearch import Elasticsearch, RequestsHttpConnection
+from requests_aws4auth import AWS4Auth
 import argparse, gzip, boto3
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--numdays', help='Number of days back from today to archive, default is 0', default=0)
     parser.add_argument('--archive', help='Archive name')
-    parser.add_argument('--esaddress', help='Elasticsearch Address', default='localhost:9200')
+    parser.add_argument('--logtype', help='Log type cwl/logstash', default='cwl')
+    parser.add_argument('--esaddress', help='Elasticsearch Address', default='localhost')
+    parser.add_argument('--esport', help='Elasticsearch Port', default=9200)
     parser.add_argument('--bucket', help='S3 bucket name')
+    parser.add_argument('--awsaccesskey', help='AWS Access Key')
+    parser.add_argument('--awssecretkey', help='AWS Secret Key')
+    parser.add_argument('--awsregion', help='AWS Region', default='us-east-1')
     parser.add_argument('--dry', help='Dry run', action='store_true')
+
     args = parser.parse_args()
 
     numDays = args.numdays
     archiveName = args.archive
     now = datetime.now()
     indexDay = int(now.day) - int(numDays)
-    indexName = 'logstash-%s.%02d.%s' % (now.year, now.month, indexDay)
+    indexName = '%s-%s.%02d.%s' % (args.logtype, now.year, now.month, indexDay)
     stuffs = []
 
-    es = Elasticsearch(hosts=args.esaddress)
+    awsauth = AWS4Auth(args.awsaccesskey, args.awssecretkey, args.awsregion, 'es')
+
+    es = Elasticsearch(
+        hosts=[{'host': args.esaddress, 'port': int(args.esport)}],
+        http_auth=awsauth,
+        use_ssl=True,
+        verify_certs=True,
+        connection_class=RequestsHttpConnection
+    )
+    print(es.info())
 
     query={"query" : {"match_all" : {}}}
 
